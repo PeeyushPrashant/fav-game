05b002dfe51a42545d64592310ae8cb37d6abd20
PeeyushPrashant <68290209+PeeyushPrashant@users.noreply.github.com>
2021-04-27 18:32:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var emojiDictionary = {
+  "⚽": "Soccer",
+  "⚾": "Baseball",
+  "🏀": "Basketball",
+  "🏈": "Rugby",
+  "🎾": "Tennis",
+  "🏏": "Cricket",
+  "🏑": "Hockey",
+  "⛳": "Golf"
+};
+
+var emojisWeKnow = Object.keys(emojiDictionary);
+export default function App() {
+  const [meaning, setMeaning] = useState("");
+
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+
+    if (meaning === undefined) {
+      meaning = "failed to recognise this emojii";
+    }
+
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+  return (
+    <div className="App">
+      <h1>Which game do you like?</h1>
+      <input onChange={emojiInputHandler}></input>
+      <h2>{meaning}</h2>
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <div
+            onClick={() => emojiClickHandler(emoji)}
+            style={{
+              fontSize: "2rem",
+              padding: "0.5rem",
+              cursor: "pointer",
+              display: "inline"
+            }}
+          >
+            {" "}
+            {emoji}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
